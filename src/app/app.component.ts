aefcc735dbe8088e86982c405ee2a6155302fc41
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-03-01 20:54:39 +0200

    add cross field validation, still problems, doesn't display error at password difference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {forbiddenNameValidator, forbiddenNameValidator2} from "./shared/user-name.validator";
+import {passwordValidator} from "./shared/password.validator";
 
 @Component({
   selector: 'app-root',
@@ -23,18 +24,19 @@ export class AppComponent {
     return this.registrationForm.get('userName');
   }
 
+
   constructor(private formBuilder: FormBuilder) {}
 
   //form builder-ul este un altfel de mod de a crea form grups/control
   public registrationForm = this.formBuilder.group({
-    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator2(/password/)]],
+    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator2(/password/), forbiddenNameValidator]],
     password: [''],
     confirmPassword: [""],
     address: this.formBuilder.group({
       city: [""],
       state: [""],
       postalCode: ['']
-    })
+    }, {validator: passwordValidator})
   })
 
   loadApiData(){
