b488957db1446150d76055b98fe9d594953e15cd
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-03-01 17:25:47 +0200

    add reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registrationForm = new FormGroup({
+    userName: new FormControl('Cosmin'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  });
   title = 'reactive-forms';
+
+  loadApiData(){
+    this.registrationForm.setValue({// completam total fromul
+      userName: "Bruce",
+      password: "test",
+      confirmPassword: "test",
+      address: {
+        city: "Gotham",
+        state: "Gotham",
+        postalCode: '123456'
+      }
+    });
+  }
+
+  loadApiData2(){
+    this.registrationForm.patchValue({// completam partial formul
+      userName: "Bruce",
+      password: "test",
+      confirmPassword: "test",
+    });
+  }
 }
